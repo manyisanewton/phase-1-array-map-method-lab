a79e18a59a18dcccef21ecf9560e6123975b47c2
Newton Manyisa <manyisanewton26@gmail.com>
2024-12-10 19:03:10 +0300

    phase-1-array-map-method-lab

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ') // Split the string into an array of words
+      .map(word => word[0].toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+      .join(' '); // Join the words back into a string
+  });
+};
+
+console.log(titleCased());
